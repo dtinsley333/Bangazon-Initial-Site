6c54b9fac4df26e77aed68e4e0c1d1f8a145d18d
Thomas J. Shavor <thomasshavor@gmail.com>
2016-10-24 10:55:21 -0500

    completed payment type model

---
+++ Models/PaymentType.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace BangazonDelta.Models
+{
+  public class PaymentType
+  //now we define what we want our table to look like...//
+  {
+    [Key]
+    //this is the Primary Key from our ERD diagram//
+    public int PaymentTypeId {get;set;}
+
+    [Required]
+    //we want AccountNumber to be required...//
+    public int AccountNumber {get;set;}
+
+    [Required]
+    //we want CVV to be required...//
+    public int CVV { get; set; }
+
+    [Required]
+    public DateTime Expiration { get; set; }
+
+    [Required]
+    public int UserId { get; set; }
+    
+  }
+}
