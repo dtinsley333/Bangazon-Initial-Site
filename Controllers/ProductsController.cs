cf05bd326aeea5038934892726bc8d44ada3f8e1
Casey Reed <casey.w.reed@gmail.com>
2016-10-25 14:56:43 -0500

    Product Detail view added. User is included with Product Detail pull

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -23,9 +23,28 @@ namespace BangazonTeamDelta.Controllers
 
         public async Task<IActionResult> ProductTypes()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View(await context.ProductType.ToListAsync());
+        }
+
+        public async Task<IActionResult> Detail([FromRoute]int? id)
+        {
+            // If no id was in the route, return 404
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await context.Product
+                    .Include(s => s.User)
+                    .SingleOrDefaultAsync(m => m.ProductId == id);
+
+            // If product not found, return 404
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
         }
 
         public IActionResult Create()
